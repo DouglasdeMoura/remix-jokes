eaf62e3e0ba2855752082b16672c704889d6894c
Douglas Moura <douglas.ademoura@gmail.com>
2021-12-05 01:16:16 -0300

    feat: enable user to add jokes

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,28 @@
-import { LiveReload } from "remix";
+import type { LinksFunction } from "remix";
+import { Links, LiveReload, Outlet } from "remix";
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)"
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)"
+    }
+  ];
+};
 
 export default function App() {
   return (
@@ -7,9 +31,10 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Remix: so great, it's funny!</title>
+        <Links />
       </head>
       <body>
-        <h1>Hello World</h1>
+        <Outlet />
         {process.env.NODE_ENV === "development" ? <LiveReload /> : null}
       </body>
     </html>
